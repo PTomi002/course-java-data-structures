f1f00f3551963b7f9bceb7fd3fae4de7f5b36bb3
paulin1tama929 <paulin.tamas@telekom.hu>
2021-08-12 15:31:41 +0200

    Fixing javadoc

--- src/main/java/hu/ptomi/Arrays.java
+++ src/main/java/hu/ptomi/Arrays.java
@@ -47,55 +47,55 @@ public class Arrays {
 
 /**
  * Hi,
- *
+ * <p>
  * I do have a question here:
- *
+ * <p>
  * int[] values = {1, 2, 3};
- *         // Object header 12 bytes
- *         // length = 4 bytes ------------------?? It's still not clear to me what a length means here? Please explain..
- *         // 3 x 4 bytes for values // No we have 3 elements * 4 that is the length. Why we are again adding +4 in final calculation?
- *         // 12 + 4 + 12 = 28 = 28 = 32 byte
- *
+ * // Object header 12 bytes
+ * // length = 4 bytes ------------------?? It's still not clear to me what a length means here? Please explain..
+ * // 3 x 4 bytes for values // No we have 3 elements * 4 that is the length. Why we are again adding +4 in final calculation?
+ * // 12 + 4 + 12 = 28 = 28 = 32 byte
+ * <p>
  * Thank you..
- *
+ * <p>
  * Hi Karan, length is a field inside array.  Remember that you can ask how many elements are inside any array with ".length".  That's 4 bytes for the 32-bit int.
- *
+ * <p>
  * Object size is always a multiple of 8 for object alignment, hence the additional padding of 4 in that calculation.
- *
+ * <p>
  * Object headers depend on what you are running on:
- *
+ * <p>
  * 32-bit Java:
  * java.lang.Object object internals:
- *  OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
- *       0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
- *       4     4       (object header)                f8 68 00 0d (11111000 01101000 00000000 00001101) (218130680)
+ * OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
+ * 0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+ * 4     4       (object header)                f8 68 00 0d (11111000 01101000 00000000 00001101) (218130680)
  * Instance size: 8 bytes
  * Space losses: 0 bytes internal + 0 bytes external = 0 bytes total
- *
+ * <p>
  * 64-bit Java with Compressed Oops (default usually nowadays):
- *
+ * <p>
  * java.lang.Object object internals:
- *  OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
- *       0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
- *       4     4       (object header)                00 00 00 00 (00000000 00000000 00000000 00000000) (0)
- *       8     4       (object header)                e1 11 5c ff (11100001 00010001 01011100 11111111) (-10743327)
- *      12     4       (loss due to the next object alignment)
+ * OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
+ * 0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+ * 4     4       (object header)                00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+ * 8     4       (object header)                e1 11 5c ff (11100001 00010001 01011100 11111111) (-10743327)
+ * 12     4       (loss due to the next object alignment)
  * Instance size: 16 bytes
  * Space losses: 0 bytes internal + 4 bytes external = 4 bytes total
- *
+ * <p>
  * 64-bit Java without Compressed Oops:
- *
+ * <p>
  * java.lang.Object object internals:
- *  OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
- *       0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
- *       4     4       (object header)                00 00 00 00 (00000000 00000000 00000000 00000000) (0)
- *       8     4       (object header)                60 8f 3e 1b (01100000 10001111 00111110 00011011) (457084768)
- *      12     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+ * OFFSET  SIZE  TYPE DESCRIPTION                    VALUE
+ * 0     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+ * 4     4       (object header)                00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+ * 8     4       (object header)                60 8f 3e 1b (01100000 10001111 00111110 00011011) (457084768)
+ * 12     4       (object header)                01 00 00 00 (00000001 00000000 00000000 00000000) (1)
  * Instance size: 16 bytes
  * Space losses: 0 bytes internal + 0 bytes external = 0 bytes total
- *
- *
+ * <p>
+ * <p>
  * Compressed Oops make the references 4 bytes large on a 64-bit machine.  Usually they would be 8 bytes.  You can use Compressed Oops up to about 32 GB or so of heap.  It is quite a bit faster than normal Oops and of course uses less memory.
- *
+ * <p>
  * Hope this helps a bit.
  */
